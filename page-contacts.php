fddc512250363cd8448814716d648281620e568e
Ruvick <rudikov.web@ya.ru>
2022-05-16 13:19:03 +0300

    Подключил карту в контактах

--- page-contacts.php
+++ page-contacts.php
@@ -85,7 +85,49 @@ get_header(); ?>
 						Если уже не оказались.»
 					</p>
 				</div>
-				<div id="map" class="page-recurring__block-map"></div>
+				<div id="mapContacts" class="page-recurring__block-map"></div>
+				<script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script> 
+
+		<script>
+			ymaps.ready(init); 
+
+			function init () {
+				var myMap = new ymaps.Map("mapContacts", {
+        // Координаты центра карты
+        center:[<?php echo carbon_get_theme_option('map_point') ?>],
+        // Масштаб карты
+        zoom: 17,
+        // Выключаем все управление картой
+        controls: []
+      }); 
+
+				var myGeoObjects = [];
+
+    	// Указываем координаты метки
+    		myGeoObjects = new ymaps.Placemark([<?php echo carbon_get_theme_option('map_point') ?>],{
+    		// hintContent: '<div class="map-hint">Авто профи, Курск, ул.Комарова, 16</div>',
+    		balloonContent: '<div class="map-hint"><?php echo carbon_get_theme_option('text_map') ?>', },{
+    		iconLayout: 'default#image',
+        // Путь до нашей картинки
+        iconImageHref:  '<?php bloginfo("template_url"); ?>/img/icons/map-marker.svg',  
+        // Размеры иконки
+        iconImageSize: [30, 35],
+        // Смещение верхнего угла относительно основания иконки
+        iconImageOffset: [-25, -100]
+      });
+
+    	var clusterer = new ymaps.Clusterer({
+    		clusterDisableClickZoom: false,
+    		clusterOpenBalloonOnClick: false,
+    	});
+    
+    	clusterer.add(myGeoObjects);
+    	myMap.geoObjects.add(clusterer);
+    	// Отключим zoom
+    	myMap.behaviors.disable('scrollZoom');
+
+  		}
+		</script>
 
 			</div>
 
